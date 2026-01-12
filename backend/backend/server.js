8413076c3b4339ba1a1fa0c3d90c31e6040bbae4
Henrietta Atsenokhai <henrietta.atsenokhai@gmail.com>
2026-01-12 06:50:10 +0100

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-
-const { initializeDatabase } = require('./config/database');
+const { initializeDatabase } = require('./backend/config/database');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
